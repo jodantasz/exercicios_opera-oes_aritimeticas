422c0b576f50d7596a1eabd7f982495dc82d3a12
joão <bielr7dantas@gmail.com>
2026-05-20 11:35:19 -0300

    first commit

---
+++ Exercicios_python.py
@@ -0,0 +1,91 @@
+#EX1
+#Calcule o total de alunos em duas turmas, uma com 28 alunos e outra com 32.
+print("==========================")
+print("Exercicio 1:")
+
+Sala1 = 28
+Sala2 = 32
+Valor_Final = Sala1 + Sala2
+print("Valor Final:", Valor_Final)
+
+#EX2
+#Um pacote de dados tem 500MB. Após baixar 150MB, quanto resta?
+print("==========================")
+print("Exercicio 2:")
+
+Pacote = 500
+Apos_Baixar = 150
+Restante = Pacote - Apos_Baixar
+print("O restante é de:", Restante)
+
+#EX3
+#Uma caixa contém 24 ovos. Quantos ovos existem em 8 caixas?
+print("==========================")
+print("Exercicio 3:")
+
+Caixa1 = 24
+Caixa2 = 24 * 8
+print(f"Existem: {Caixa2} em 8 Caixas")
+
+#EX4
+#Divida 10 litros de suco igualmente entre 3 pessoas e mostre o resultado.
+print("==========================")
+print("Exercicio 4:")
+
+suco = 10
+divisao = 10 / 3
+print(f" Resultado: {divisao}")
+
+#EX5
+#Use divisão inteira para descobrir quantas pessoas recebem 3 litros inteirosdos 10 litros disponíveis.
+print("==========================")
+print("Exercicio 5:")
+
+suco = 10
+divisao = 10 / 3
+print("resultado: ", int(divisao))
+
+#EX6
+#Calcule o resto da divisão de 10 por 3.
+print("==========================")
+print("Exercicio 6:")
+
+suco = 10
+divisao = 10 % 3
+print(f"resultado: {divisao}")
+
+#EX7
+# Calcule 2 elevado à potência 8.
+print("==========================")
+print("Exercicio 7:")
+
+calculo = 2 ** 8
+print("O Calculo deu: ", calculo)
+
+#EX8
+# Calcule a raiz quadrada de 81 usando exponenciação.
+
+print("==========================")
+print("Exercicio 8:")
+raiz_quadrada = 81 ** 0.5
+print("Resultado", raiz_quadrada)
+
+#EX9
+#Calcule a expressão:(10 + 5) * 2 - 8 / 4
+print("==========================")
+
+print("Exercicio 9:")
+conta = (10 + 5) * 2 - 8 / 4
+print("O resultado da conta:", conta)
+
+#EX10
+#Mostre o resultado da divisão de -7 por usando divisão inteira e o operador de resto.
+print("==========================")
+
+print("Exercicio 10:")
+calculo = -7 // 2
+resto = -7 % 2
+print("Divisao Inteira:", calculo)
+print("Resto da divisão", resto)
+
+print("==========================")
